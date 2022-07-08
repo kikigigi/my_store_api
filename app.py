cee6a84dbec9a6df9a062cfbc8a22de563d0751c
kikigigi <momokocute1@gmail.com>
2022-07-08 20:14:36 +0100

    Add /store end point

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 import os
 
@@ -9,6 +9,15 @@ app.secret_key = os.getenv('SECRET_KEY')
 @app.route('/')
 def home():
     return 'Hello World From Me!'
+
+
+@app.route('/store', methods=['POST'])
+def create_store():
+    data = request.get_json()
+    print(data)
+    return jsonify({'name': data['name']})
+
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
